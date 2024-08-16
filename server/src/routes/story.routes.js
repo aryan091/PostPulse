658eebb71096983494bdba2ce2717f4f559c984e
Aryan Daftari <aryandaftari7@gmail.com>
2024-08-16 12:51:13 +0530

    Added Routes for Story

--- server/src/routes/story.routes.js
+++ server/src/routes/story.routes.js
@@ -3,12 +3,11 @@ const router = express.Router()
 const postController = require('../controllers/post.controller.js')
 const {verifyToken} = require('../middlewares/verifyJwtToken')
 
-router.post( "/create", verifyToken , postController.createStory)
-router.get( "/my-stories", verifyToken , postController.getMyStories)
-router.put( "/update/:storyId", verifyToken , postController.updateStory)
-router.get( "/all-stories" , postController.getAllStories)
-router.get( "/category/:category" , postController.getStoriesByCategory)
-router.get( "/view-story/:storyId" , postController.viewStory)
+router.post( "/create", verifyToken , postController.createPost)
+router.get( "/my-stories", verifyToken , postController.getMyPosts)
+router.put( "/update/:postId", verifyToken , postController.updatePost)
+router.get( "/all-stories" , postController.getAllPosts)
+router.get( "/view-story/:postId" , postController.viewPost)
 
 
 
