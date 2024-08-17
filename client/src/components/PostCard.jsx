b298e635a0780db2f61c0af1d700371160620cee
Aryan Daftari <aryandaftari7@gmail.com>
2024-08-17 15:47:21 +0530

    Design Post Card for Rendering Posts

--- client/src/components/PostCard.jsx
+++ client/src/components/PostCard.jsx
@@ -1,16 +1,22 @@
 import React from 'react';
-import { BG_CARD_URL } from '../utils/constants';
+import { BG_CARD_URL , AVATAR_URL} from '../utils/constants';
 import { FcLike } from "react-icons/fc";
 import { FaBookmark, FaEdit } from "react-icons/fa";
 
-const PostCard = () => {
+const PostCard = ({post}) => {
+
+    console.log("Post : ",post)
+
+    const { heading, description, imageUrl } = post
+    
+
   return (
     <div className="relative h-[460px] w-80 rounded-md overflow-hidden group shadow-2xl">
       {/* Background Image */}
       <div
         className="absolute inset-0 transition-transform duration-300 ease-in-out bg-cover bg-center group-hover:scale-110"
         style={{ 
-          backgroundImage: `url(${BG_CARD_URL})`,
+          backgroundImage: `url(${imageUrl})`,
           backgroundSize: 'cover',
           backgroundPosition: 'center',
           zIndex: -1
@@ -23,13 +29,23 @@ const PostCard = () => {
 
       {/* Content */}
       <div className="relative z-10 bottom-2 left-0 right-0 p-4 bg-gradient-to-b from-black rounded-b-md">
-        <h1 className="text-4xl font-bold text-white mt-72">Elemental</h1>
-        <p className="text-white mt-2 line-clamp-3 overflow-hidden">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos
-          reiciendis nobis modi? Natus consequatur nisi nostrum. Provident
-          atque repellat debitis enim eius corporis labore, laboriosam iure
-          architecto repellendus praesentium? Voluptatibus!
+        <h1 className="text-[2rem] font-bold text-white mt-52  line-clamp-1 overflow-hidden">{heading}</h1>
+        <p className="text-white h-12 mt-2 line-clamp-2 overflow-hidden">
+          {description}
         </p>
+
+        <div className="text-white flex justify-between mt-2">
+          <div className="flex flex-row gap-2 items-center ">
+            <div className='rounded-full w-12 h-12'>
+                <img src={AVATAR_URL} alt=""  className='rounded-full w-full h-full'/>
+                </div>            
+            <div className='text-white font-bold'>Aryan Daftari</div>
+          </div>
+        </div>
+
+        <p className='text-white font-semibold mt-2'>Published on 01 Jan 2023</p>
+
+
         <div className="text-white flex justify-between mt-2">
           <div className="flex flex-row gap-2 items-center">
             <button><FcLike size={24} /></button>
@@ -37,6 +53,7 @@ const PostCard = () => {
           </div>
           <button><FaBookmark size={24} color="blue" /></button>
         </div>
+
       </div>
     </div>
   );
--- client/src/components/PostList.jsx
+++ client/src/components/PostList.jsx
@@ -1,9 +1,40 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Header from './Header';
 import { BG_URL } from '../utils/constants';
 import PostCard from './PostCard';
+import { useDispatch , useSelector } from 'react-redux';
+import axios from 'axios';
+import { toast } from 'react-toastify';
+import { setPosts } from '../slice/postSlice';
 
 const PostList = () => {
+
+    const dispatch = useDispatch();
+    const fetchAllPosts = async () => {
+        try {
+            const reqUrl = `${import.meta.env.VITE_BACKEND_URL}/post/all-posts`;
+            const token = localStorage.getItem('token');
+            axios.defaults.headers.common['Authorization'] = token;
+            const response = await axios.get(reqUrl);
+        
+            console.log(response.data.data);
+            dispatch(setPosts(response.data.data.posts));
+        
+        
+        } catch (error) {
+            console.log(error);
+            
+        }
+        }
+        
+        useEffect(() => {
+          fetchAllPosts();
+        }, []);
+
+        const posts = useSelector((state) => state?.post?.posts);
+        console.log(posts);
+
+
   return (
     <div className="relative h-screen overflow-hidden">
       <Header />
@@ -15,8 +46,11 @@ const PostList = () => {
 
       {/* Post Cards */}
       <div className="relative z-10 flex flex-wrap justify-center gap-4 py-20 overflow-y-scroll h-full mt-16">
-        <PostCard />
-        <PostCard />    <PostCard />    <PostCard />    <PostCard />
+       {
+        posts && posts.map((post) => (
+          <PostCard post={post} />
+        ))
+       }
       </div>
     </div>
   );
