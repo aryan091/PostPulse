44fc98c641337a9b650f1106f54010e9e1b9a1d9
Aryan Daftari <aryandaftari7@gmail.com>
2024-08-22 01:10:36 +0530

    Added Comment Model and Made Changes in Post Model Acc

--- server/src/models/post.model.js
+++ server/src/models/post.model.js
@@ -1,50 +1,46 @@
-const e = require("express");
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
-const postSchema = new mongoose.Schema(
-  {
-    heading: {
-      type: String,
-      required: true,
-    },
-    description: {
-      type: String,
-      required: true,
-    },
-    imageUrl: {
-      type: String,
-      required: true,
-    },
 
-    likes: [
-      {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "User",
-        email: String,
-      },
-    ],
-    bookmarks: [
-      {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "User",
-        email: String,
-
-      },
-    ],
-
-    totalLikes: {
-      type: Number,
-      default: 0,
-    },
-
-    addedBy: {
-      type: String,
-      required: true,
-      default: "Unknown",
-    },
-
- 
+const postSchema = new mongoose.Schema({
+  heading: {
+    type: String,
+    required: true,
   },
-  { timestamps: true }
-);
-module.exports = mongoose.model("Post", postSchema);
+  description: {
+    type: String,
+    required: true,
+  },
+  imageUrl: {
+    type: String,
+    required: true,
+  },
+  likes: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+    },
+  ],
+  bookmarks: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+    },
+  ],
+  totalLikes: {
+    type: Number,
+    default: 0,
+  },
+  addedBy: {
+    type: String,
+    required: true,
+    default: 'Unknown',
+  },
+  comments: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Comment',
+    },
+  ],
+}, { timestamps: true });
+
+module.exports = mongoose.model('Post', postSchema);
