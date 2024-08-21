469de6a3bee2703a2dbecd695bf40a4c69fb8cb4
Aryan Daftari <aryandaftari7@gmail.com>
2024-08-22 01:13:03 +0530

    Added comment in Store

--- client/src/slice/viewPostSlice.js
+++ client/src/slice/viewPostSlice.js
@@ -1,11 +1,13 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+
 const postSlice = createSlice(
     {
         name: "viewPost",
         initialState: {
             post: null,
-        totalLikes : null
+        totalLikes : null,
+        comments:null
         },
         reducers: {
             setPost: (state, action) => {
@@ -16,10 +18,28 @@ const postSlice = createSlice(
             },
             updatePost : (state, action) => {
                 state.post = action.payload;
+            },
+            setComment : (state, action) => {
+                state.comments = action.payload
+            },
+            addComment(state, action) {
+                state.comments = [...state.comments, action.payload]
+            },
+            updateComment(state, action) {
+                state.comments = state.comments.map((comment) => {
+                    if (comment._id === action.payload._id) {
+                        return action.payload
+                    }
+                    return comment
+                })
+            },
+            deleteComment(state, action) {
+                state.comments = state.comments.filter((comment) => comment._id !== action.payload)
             }
+
         },
     }
 );
 
-export const { setPost , updatePost , setTotalLikes} = postSlice.actions;
+export const { setPost , updatePost , setComment , setTotalLikes , addComment , updateComment , deleteComment} = postSlice.actions;
 export default postSlice.reducer;
