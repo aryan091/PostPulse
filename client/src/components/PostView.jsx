eee6b99935fe746dfe736f895982ae7479575a64
Aryan Daftari <aryandaftari7@gmail.com>
2024-08-22 17:51:48 +0530

    Improved Performance

--- client/src/components/PostView.jsx
+++ client/src/components/PostView.jsx
@@ -17,7 +17,7 @@ import { UserContext } from "../context/UserContext";
 
 const PostView = () => {
   const { postId } = useParams();
-  
+
   const posts = useSelector((state) => state?.post?.posts);
   const { post, loading: postLoading, error } = useFetchPost(postId, posts);
 
@@ -25,7 +25,7 @@ const PostView = () => {
   const { loading: commentsLoading } = useFetchComments(postId);
   const postLoadingState = postLoading || userLoading || commentsLoading;
 
-  const { email , isUserLoggedIn } = useContext(UserContext);
+  const { email, isUserLoggedIn } = useContext(UserContext);
 
   const commentsFromStore = useSelector((state) => state?.viewPost?.comments);
   const {
@@ -37,10 +37,6 @@ const PostView = () => {
     handleDeleteComment,
     loading,
   } = useCommentActions(postId);
-
-  if (postLoadingState) {
-    return <ShimmerView />;
-  }
 
   if (error) {
     return <div>Error loading post.</div>;
@@ -67,136 +63,139 @@ const PostView = () => {
           />
         </div>
 
-        <div className="relative z-10 bg-black bg-opacity-70 md:mx-auto h-full md:h-3/4 top-20 text-white w-screen md:w-4/5 shadow-2xl rounded-lg">
-          <div className="w-full h-1/4 md:h-full flex flex-col">
-            <div className="w-full h-96">
-              <img
-                src={imageUrl}
-                alt=""
-                className="w-full h-full object-fill rounded-lg"
-              />
-            </div>
+        {!postLoading && (
+          <div className="relative z-10 bg-black bg-opacity-70 md:mx-auto h-full md:h-3/4 top-20 text-white w-screen md:w-4/5 shadow-2xl rounded-lg">
+            <div className="w-full h-1/4 md:h-full flex flex-col">
+              <div className="w-full h-96">
+                <img
+                  src={imageUrl}
+                  alt=""
+                  className="w-full h-full object-fill rounded-lg"
+                />
+              </div>
 
-            <div className="h-full w-full p-4 md:p-8">
+              <div className="h-full w-full p-4 md:p-8">
+                <div
+                  className={`text-white flex justify-between ${
+                    !isUserLoggedIn ? "flex-row-reverse" : ""
+                  } mt-2 gap-4 md:gap-0`}
+                >
+                  {isUserLoggedIn && (
+                    <div className="flex flex-row gap-2 items-center">
+                      <div className="rounded-full w-10 h-10">
+                        <img
+                          src={AVATAR_URL}
+                          alt=""
+                          className="rounded-full w-full h-full"
+                        />
+                      </div>
+                      <div>
+                        <div className="text-white font-bold">{user?.name}</div>
+                        <div className="text-white font-semibold">
+                          {user?.email}
+                        </div>
+                      </div>
+                    </div>
+                  )}
 
-            <div className={`text-white flex justify-between ${!isUserLoggedIn ? 'flex-row-reverse' : ''} mt-2 gap-4 md:gap-0`}>
-
-              { isUserLoggedIn &&(<div className="flex flex-row gap-2 items-center">
-                <div className="rounded-full w-10 h-10">
-                  <img
-                    src={AVATAR_URL}
-                    alt=""
-                    className="rounded-full w-full h-full"
-                  />
-                </div>
-                <div>
-                  <div className="text-white font-bold">{user?.name}</div>
-                  <div className="text-white font-semibold">
-                    {user?.email}
+                  <div>
+                    <p className="text-white  font-semibold mt-2">
+                      Published on {date}
+                    </p>
                   </div>
                 </div>
-              </div>
-)}
 
-<div>
-<p className="text-white  font-semibold mt-2">
-                  Published on {date}
+                <h1 className="text-[2rem] font-bold text-white mt-4 md:mt-8">
+                  {heading}
+                </h1>
+                <p className="text-white mt-2 md:mt-4 text-justify">
+                  {description}
                 </p>
               </div>
             </div>
 
-              <h1 className="text-[2rem] font-bold text-white mt-4 md:mt-8">
-                {heading}
-              </h1>
-              <p className="text-white mt-2 md:mt-4 text-justify">
-                {description}
-              </p>
-            </div>
+            <div className="p-4 bg-gray-800 text-white bg-opacity-70">
+              <h2 className="text-xl font-bold mb-4">Comments</h2>
+              <div className="mb-4">
+                {loading ? (
+                  <ShimmerComment />
+                ) : commentsFromStore?.length === 0 ? (
+                  <p>No comments yet. Be the first to comment!</p>
+                ) : (
+                  commentsFromStore?.map((comment) => (
+                    <div
+                      key={comment?._id}
+                      className="mb-4 p-2 border-b border-gray-600"
+                    >
+                      <div className="flex justify-end gap-2 z-20">
+                        {isUserLoggedIn && (
+                          <>
+                            <button
+                              className="z-20"
+                              onClick={(e) =>
+                                handleDeleteComment(comment._id, e)
+                              }
+                            >
+                              <RiDeleteBin6Fill size={24} color="red" />
+                            </button>
 
-          </div>
+                            {email === comment?.commentedBy?.email && (
+                              <button
+                                className="z-20"
+                                onClick={(e) => handleEditComment(comment, e)}
+                              >
+                                <FaEdit size={24} color="white" />
+                              </button>
+                            )}
+                          </>
+                        )}
+                      </div>
 
-
-
-
-          <div className="p-4 bg-gray-800 text-white bg-opacity-70">
-            <h2 className="text-xl font-bold mb-4">Comments</h2>
-            <div className="mb-4">
-              {loading ? (
-                <ShimmerComment />
-              ) : commentsFromStore?.length === 0 ? (
-                <p>No comments yet. Be the first to comment!</p>
-              ) : (
-                commentsFromStore?.map((comment) => (
-                  <div
-                    key={comment?._id}
-                    className="mb-4 p-2 border-b border-gray-600"
-                  >
-                    <div className="flex justify-end gap-2 z-20">
-      {isUserLoggedIn && (
-        <>
-          <button
-            className="z-20"
-            onClick={(e) => handleDeleteComment(comment._id, e)}
-          >
-            <RiDeleteBin6Fill size={24} color="red" />
-          </button>
-          
-          {email === comment?.commentedBy?.email && (
-            <button
-              className="z-20"
-              onClick={(e) => handleEditComment(comment, e)}
-            >
-              <FaEdit size={24} color="white" />
-            </button>
-          )}
-        </>
-      )}
-    </div>
-
-                    <div>
-                      <div className="flex flex-row gap-2 items-center">
-                        <div className="rounded-full w-10 h-10">
-                          <img
-                            src={AVATAR_URL}
-                            alt=""
-                            className="rounded-full w-full h-full"
-                          />
-                        </div>
-                        <div>
-                          <div className="text-white font-bold">
-                            {comment?.commentedBy?.name}
+                      <div>
+                        <div className="flex flex-row gap-2 items-center">
+                          <div className="rounded-full w-10 h-10">
+                            <img
+                              src={AVATAR_URL}
+                              alt=""
+                              className="rounded-full w-full h-full"
+                            />
                           </div>
-                          <div className="text-white text-sm font-semibold">
-                            {comment?.commentedBy?.email}
+                          <div>
+                            <div className="text-white font-bold">
+                              {comment?.commentedBy?.name}
+                            </div>
+                            <div className="text-white text-sm font-semibold">
+                              {comment?.commentedBy?.email}
+                            </div>
                           </div>
                         </div>
                       </div>
+                      <p className="mt-4">{comment?.text}</p>
+                      <p className="text-sm text-end font-bold text-gray-400">
+                        Published on {formatDate(comment?.createdAt)}
+                      </p>
                     </div>
-                    <p className="mt-4">{comment?.text}</p>
-                    <p className="text-sm text-end font-bold text-gray-400">
-                      Published on {formatDate(comment?.createdAt)}
-                    </p>
-                  </div>
-                ))
-              )}
-            </div>
+                  ))
+                )}
+              </div>
 
-            <form onSubmit={(e) => handleCommentSubmit(e)}>
-              <textarea
-                className="w-full p-2 mb-2 rounded bg-gray-700 text-white"
-                placeholder="Write a comment..."
-                value={newComment}
-                onChange={(e) => setNewComment(e.target.value)}
-              ></textarea>
-              <button
-                type="submit"
-                className="mt-2 p-2 bg-transparent border border-gray-600 text-white rounded-md w-full"
-              >
-                {editCommentId ? "Update Comment" : "Add Comment"}
-              </button>
-            </form>
+              <form onSubmit={(e) => handleCommentSubmit(e)}>
+                <textarea
+                  className="w-full p-2 mb-2 rounded bg-gray-700 text-white"
+                  placeholder="Write a comment..."
+                  value={newComment}
+                  onChange={(e) => setNewComment(e.target.value)}
+                ></textarea>
+                <button
+                  type="submit"
+                  className="mt-2 p-2 bg-transparent border border-gray-600 text-white rounded-md w-full"
+                >
+                  {editCommentId ? "Update Comment" : "Add Comment"}
+                </button>
+              </form>
+            </div>
           </div>
-        </div>
+        )}
       </div>
       <Footer />
     </>
