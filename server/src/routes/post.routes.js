1c6f675cf92174a489b323c5aac67cdff6a8ff23
Aryan Daftari <aryandaftari7@gmail.com>
2024-08-24 05:11:17 +0530

    Updated Routes Acc to Image Upload

--- server/src/routes/post.routes.js
+++ server/src/routes/post.routes.js
@@ -3,10 +3,21 @@ const router = express.Router()
 const postController = require('../controllers/post.controller.js')
 const likeController = require('../controllers/like.controller.js')
 const {verifyToken} = require('../middlewares/verifyJwtToken.js')
+const {upload} = require('../middlewares/multer.middleware.js')
 
-router.post( "/create", verifyToken , postController.createPost)
+router.post(
+    '/create',
+    verifyToken,
+    upload.fields([{ name: 'imageAvatar', maxCount: 1 }]), // Handle imageAvatar upload
+    postController.createPost
+  );
 router.get( "/my-posts", verifyToken , postController.getMyPosts)
-router.put( "/update/:postId", verifyToken , postController.updatePost)
+router.put(
+    '/update/:postId',
+    verifyToken,
+    upload.fields([{ name: 'imageAvatar', maxCount: 1 }]), // Handle imageAvatar upload
+    postController.updatePost
+  );
 router.get( "/all-posts" , postController.getAllPosts)
 router.get( "/view-post/:postId" , postController.viewPost)
 router.delete( "/delete/:postId", verifyToken , postController.deletePost)
--- server/src/routes/user.routes.js
+++ server/src/routes/user.routes.js
@@ -3,8 +3,13 @@ const router = express.Router()
 const authController = require('../controllers/user.controller.js')
 const { verifyToken } = require('../middlewares/verifyJwtToken.js')
 const bookmarkController = require('../controllers/bookmark.controller.js')
+const {upload} = require('../middlewares/multer.middleware.js')
 
-router.post( "/register", authController.registerUser )
+router.post(
+    "/register",
+    upload.single('avatar'),
+    authController.registerUser
+  );
 router.post( "/login", authController.loginUser )
 router.get('/profile' , verifyToken, authController.getUserProfile)
 router.get('/:userId' , verifyToken, authController.getUserById)
